4a7ed9b6d32c52dea0edb6216436de0faa802df5
wxg <wuxueguang@outlook.com>
2020-04-22 20:38:55 +0800

    ...

--- dom-leaning/refer&async/defer1.js
+++
@@ -1,1 +0,0 @@
-console.log('defer script 1');
--- dom-leaning/tasks/setInterval.js
+++
@@ -1,6 +0,0 @@
-console.log('interval');
-let count = 0;
-let si = setInterval(() => {
-    console.log('interval', count++);
-    clearInterval(si);
-}, 0);
