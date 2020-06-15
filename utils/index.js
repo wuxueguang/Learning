da1767997b0a9e75d2481f14c345008172bb4dbe
吴学广 <wuxueguang@outlook.com>
2020-06-15 10:29:53 +0800

    ...

--- utils/index.js
+++ utils/index.js
@@ -53,10 +53,18 @@ const img2Base64 = file => {
   });
 };
 
+const isAbsoluteURL = url => {
+  // A URL is considered absolute if it begins with "<scheme>://" or "//" (protocol-relative URL).
+  // RFC 3986 defines scheme name as a sequence of characters beginning with a letter and followed
+  // by any combination of letters, digits, plus, period, or hyphen.
+  return /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url);
+};
+
 export {
   isUrl,
   key2value,
   downloadWithUrl,
   downloadWithBinary,
-  img2Base64
+  img2Base64,
+  isAbsoluteURL,
 };
